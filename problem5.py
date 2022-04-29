a570c96f8d97d5f0b405fe5a87eeafbaf078b930
yaminipilli <103175054+yaminipilli@users.noreply.github.com>
2022-04-29 17:49:37 +0530

    Update and rename problem 5 to problem5.py

--- problem5.py
+++ problem5.py
@@ -1,8 +1,6 @@
-This problem was asked by Jane Street.
+"""cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 
-cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
-
-solution:
+solution:"""
 
 def cons(a, b):
     def pair(f):
