89cf4e715d5874b93b6ca74d7b314f9801a0c0fc
yaminipilli <103175054+yaminipilli@users.noreply.github.com>
2022-06-09 08:54:29 +0530

    Add files via upload

---
+++ problem37.py
@@ -0,0 +1,26 @@
+"""The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+"""
+
+def findPowerSet(S):
+ 
+    N = int(pow(2, len(S)))
+    s = set()
+
+    for i in range(N):
+        
+        for j in range(len(S)):
+            
+            # if j'th bit of `i` is set, print `S[j]`
+            if i & (1 << j):
+                s.add(S[j])
+ 
+        print(s)
+        s.clear()
+ 
+ 
+S = [1, 2, 3]
+findPowerSet(S)
+
