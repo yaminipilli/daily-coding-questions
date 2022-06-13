1d4fcda5e0b1f60251fb67f8279e32c00be45e80
yaminipilli <103175054+yaminipilli@users.noreply.github.com>
2022-06-13 09:53:36 +0530

    Add files via upload

---
+++ problem41.py
@@ -0,0 +1,35 @@
+"""Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. All flights must be used in the itinerary.
+
+For example, given the list of flights [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] and starting airport 'YUL', you should return the list ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
+
+Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 'COM', you should return null.
+
+Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
+"""
+
+def printPath(dictionary, src):
+    dest = dictionary.get(src)
+    if not dest:
+        return
+ 
+    print(src + ' ,' + dest)
+    printPath(dictionary, dest)
+ 
+ 
+def findPath(airports):
+ 
+    destinations = {*airports.values()}
+ 
+    for k, v in airports.items():
+    
+        if k not in destinations:
+            printPath(airports, k)
+            return
+ 
+airports = {'SFO': 'HKO', 
+            'YYZ':'SFO', 
+            'YUL': 'YYZ', 
+            'HKO': 'ORD',}
+ 
+findPath(airports)
+ 
